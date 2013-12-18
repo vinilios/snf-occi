d7c70e5cba7fad5e1d2bf527c6385e9fc0633dac
Nasia Asiki <nassia.a@gmail.com>
2013-12-18 16:44:36 +0200

    Fix in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='OCCI to Openstack/Cyclades API bridge',
     url='http://code.grnet.gr/projects/snf-occi',
     license='BSD',
-    packages = ['snfOCCI','snfOCCI.snf_voms','snfOCCI.httpd','snfOCCI.snfServer'],
+    packages = ['snfOCCI','snfOCCI.snf_voms'],
     entry_points = ''' 
         [paste.app_factory]
         snf_occi_app = snfOCCI:main
