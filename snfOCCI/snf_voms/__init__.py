d152c379424f5708641991bb0f9dbc50a78b4c05
nasia <nassia.a@gmail.com>
2014-07-10 13:28:06 +0300

    change in error handling

--- snfOCCI/snf_voms/__init__.py
+++ snfOCCI/snf_voms/__init__.py
@@ -38,6 +38,51 @@ CA_PATH = "/etc/grid-security/certificates/"
 VOMSAPI_LIB = "/usr/lib/libvomsapi.so.1"
 PARAMS_ENV = 'snf_voms.params'
 
+class VomsError():
+    """Voms credential management error"""
+
+    errors = {
+        0: ('none', None),
+        1: ('nosocket', 'Socket problem'),
+        2: ('noident', 'Cannot identify itself (certificate problem)'),
+        3: ('comm', 'Server problem'),
+        4: ('param', 'Wrong parameters'),
+        5: ('noext', 'VOMS extension missing'),
+        6: ('noinit', 'Initialization error'),
+        7: ('time', 'Error in time checking'),
+        8: ('idcheck', 'User data in extension different from the real'),
+        9: ('extrainfo', 'VO name and URI missing'),
+        10: ('format', 'Wrong data format'),
+        11: ('nodata', 'Empty extension'),
+        12: ('parse', 'Parse error'),
+        13: ('dir', 'Directory error'),
+        14: ('sign', 'Signature error'),
+        15: ('server', 'Unidentifiable VOMS server'),
+        16: ('mem', 'Memory problems'),
+        17: ('verify', 'Generic verification error'),
+        18: ('type', 'Returned data of unknown type'),
+        19: ('order', 'Ordering different than required'),
+        20: ('servercode', 'Error from the server'),
+        21: ('notavail', 'Method not available'),
+    }
+
+    http_codes = {
+        5: (400, "Bad Request"),
+        11: (400, "Bad Request"),
+        14: (401, "Not Authorized"),
+    }
+
+    def __init__(self, code):
+        short, message = self.errors.get(code, ('oops',
+                                                'Unknown error %d' % code))
+        message = "(%s, %s)" % (code, message)
+        super(VomsError, self).__init__(message=message)
+
+        code, title = self.http_codes.get(code, (500, "Unexpected Error"))
+        self.code = code
+        self.title = title
+
+
 class VomsAuthN():
     """Filter that checks for the SSL data in the reqest.
 
